6949542da804f44da7b5ac62d219d641db221d9f
trankhoadev <trankhoadev@gmail.com>
2022-10-31 10:12:46 +0700

    Update blank project and setup dependencies

---
+++ spring-redis-buoi8/src/main/java/com/trananhkhoa_19501151/springredisbuoi8/SpringRedisBuoi8Application.java
@@ -0,0 +1,13 @@
+package com.trananhkhoa_19501151.springredisbuoi8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisBuoi8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisBuoi8Application.class, args);
+    }
+
+}
---
+++ spring-redis-buoi8/src/test/java/com/trananhkhoa_19501151/springredisbuoi8/SpringRedisBuoi8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trananhkhoa_19501151.springredisbuoi8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisBuoi8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
